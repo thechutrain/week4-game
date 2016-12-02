b351d6833334e27b74b3b8f8415eea33f79ce253
thechutrain <chu.chuyalan@gmail.com>
2016-12-02 15:37:29 -0500

    working beta version, no styling, no header text or footer, but game works. May need to add some css transitions etc to make it look nice

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,14 @@
-// $("h1").html("Hi");
-// alert("hi");
-
 // initialize variables
-let totalWins = 1;
-let totalLosses = 2;
+let totalWins = 0;
+let totalLosses = 0;
 let userPoints = 0;
 let goalPoints = null;
 let lives = 9;
 
-let goal_MAX = 120;
+let goal_MAX = 99;
 let goal_MIN = 19;
-let kitten_MAX = 12;
+let kitten_MAX = 19;
 let kitten_MIN = 1;
-
 
 
 // Functions
@@ -23,15 +19,13 @@ let kitten_MIN = 1;
 
     // 1.) get a random number for goal Points
     goalPoints = Math.floor(Math.random() * (goal_MAX - goal_MIN) + goal_MIN);
-    // console.log(goalPoints);
       //1a) Display this goal points
       $("#goalPoints").html(goalPoints);
 
     // 2.) Select all the kittens
     let all_kittens = $(".kitten");
-    // console.log(all_kittens);
 
-    // 3.) loop through each one,
+    // 3.) loop through each one kitten in the array
     // assigning a random data value to each one.
     for (var i=0; i < all_kittens.length; i++){
       let kittenPoints = Math.floor(Math.random() * (kitten_MAX - kitten_MIN) + kitten_MIN);
@@ -48,37 +42,42 @@ let kitten_MIN = 1;
     $("#lives").html(lives);
   }
 
-// Event listeners 
+  function displayMessage(message){
+    $("#messages").html(message);
+  }
+
+// ---- Event listeners ----
+$(document).ready(function(){
+  initializeGame();
+  displayMessage("Let's play!");
+});
+
 $(".kitten").on("click", function(){
   //0) subtract the lives
   lives --;
-
   // 1) get the points associated with the cat & add to the users points
-  // console.log($(this).data("points"));
   userPoints += $(this).data("points");
-
   //2) update the display
   updateView();
-
   //3) check if user won OR user lost OR game is not over ...
   if(userPoints < goalPoints && lives > 0){
     //3a) if game is not over, return!
     return false;
   } else if(userPoints === goalPoints && lives >0){
     //3b) if user won --> increment wins, display they won, & reset game
-    console.log("YOU won");
+    displayMessage("You won");
     totalWins ++;
   } else{
     //3c) if user lost --> increment losses, display they lost & reset game
-    console.log("You lost");
+    displayMessage("You lost");
     totalLosses ++;
   }
- 
  // 4) reset game
- console.log("GAME DONE");
+ initializeGame();
+ displayMessage("New Game");
 })
 
 
 // ---- TESTING ----
-initializeGame();
+// initializeGame();
 
